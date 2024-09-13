bed6c0a5a85ab8b92be0da80635e5f61956f2c5e
森田健太 <moritakenta@moritakentas-MacBook-Pro.local>
2024-09-13 22:28:01 +0900

    First commit

---
+++ SummonShogiClassicOnlineBattleEdition/SummonShogiClassicOnlineBattleEdition/SummonShogiClassicOnlineBattleEditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummonShogiClassicOnlineBattleEditionApp.swift
+//  SummonShogiClassicOnlineBattleEdition
+//
+//  Created by 森田健太 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SummonShogiClassicOnlineBattleEditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
